85975bde73be8b32c2305a72b8e9a35ffcba5251
Jhozefcd <jhozefc11@gmail.com>
2024-09-14 07:53:31 -0500

    Refactor complete for quiz

--- src/Store.java
+++ src/Store.java
@@ -1,19 +1,33 @@
 public class Store {
     public static void main(String[] args) {
-        int p1 = 15;
-        int p2 = 10;
-        int p3 = 5;
+       final int PRODUCTO_1 = 15;
+       final int PRODUCTO_2 = 10;
+       final int PRODCUTO_3 = 5;
 
-        int total1 = p1 * 2;
-        int total2 = p2 * 3;
-        int total3 = p3 * 4;
+       final int CANTIDAD_PRODUCTO_1 = 2;
+       final int CANTIDAD_PRODUCTO_2 = 3;
+       final int CANTIDAD_PRODUCTO_3 = 4;
 
-        int totalSales = total1 + total2 + total3;
 
-        if (totalSales > 50) {
+       
+       int[] Precios = {PRODUCTO_1, PRODUCTO_2, PRODCUTO_3};
+        int[] Cantidades = {CANTIDAD_PRODUCTO_1, CANTIDAD_PRODUCTO_2, CANTIDAD_PRODUCTO_3};
+        int totalVentas = calculateTotalVentas(Precios, Cantidades);
+        evaluateSalesPerformance(totalVentas);
+    }
+     static int calculateTotalVentas(int[] Precios, int[] Cantidades) {
+        int total = 0;
+        for (int i = 0; i < Precios.length; i++) {
+            total += Precios[i] * Cantidades[i];
+        }
+        return total;
+    }
+    static void evaluateSalesPerformance(int totalVentas) {
+        if (totalVentas > 50) {
             System.out.println("Good sales performance");
         } else {
             System.out.println("Low sales performance");
         }
     }
+
 }
